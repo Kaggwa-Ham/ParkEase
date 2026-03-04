3b23673d96a1514a6cfaaaf82ad4d900f730dc64
Kaggwa Ham <hamkaggwa727@gmail.com>
2026-03-04 16:39:30 +0300

    I had poorly names a file so i have changed it's name

---
+++ javaScript/validation.js
@@ -0,0 +1,28 @@
+//I am going to write Validation for the regisstration page
+
+let form = document.getElementById("registrationForm")
+let driverName = document.getElementById("driverName");
+let numberPlate = document.getElementById("numberPlate");
+let vehicleModel = document.getElementById("vehicleModel");
+let arrivalTime = document.getElementById("arrivalTime");
+let phoneNumber = document.getElementById("phoneNumber");
+let ninNumber = document.getElementById("ninNumber");
+let message = document.getElementById("message");
+
+form.addEventListener("submit", function(event) {
+    event.preventDefault()
+    let numberPlateRegex = /^([A-Z]{3})(\s)(\d{3})([A-Z])$/;
+    let phoneNumberRegex = /^(?:\+256|0)7\d{8}$/
+    let ninNumberRegex = /^[CF][MF]\d{5}[A-Z0-9]{7}$/;
+
+    let numberPlateInput = numberPlate.value.trim();
+    
+    if (!numberPlateRegex.test(numberPlateInput)){
+        message.textContent = "Enter valid plate number";
+        message.style.color = "red";
+        return
+    }
+    message.textContent = "Plate number is valid";
+    message.style.color = "green";
+})
+
